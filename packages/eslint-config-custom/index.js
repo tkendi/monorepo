a16408f2977bcfb4b994795831bed8967af1ce4a
ChayMyungjune <acg70159@gmail.com>
2023-11-12 22:46:16 +0900

    [FEAT] Feat turbo build seperate and eslint update

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,28 +1,23 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "turbo",
-    "next",
-    "prettier",
-    "plugin:import/typescript",
-  ],
-  ignorePatterns: [
-    ".next/*",
-    ".husky/*",
-    "next.config.js",
-    "extension"
-  ],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "import/order": [
-      "error",
-      {
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: false,
-        },
-      },
+    parser: "@typescript-eslint/parser",
+    extends: ["turbo", "next", "prettier", "plugin:import/typescript"],
+    ignorePatterns: [
+        ".next/*",
+        ".husky/*",
+        "next.config.js",
+        "extension"
     ],
-  },
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        "import/order": [
+            "error",
+            {
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: false,
+                },
+            },
+        ],
+    },
 };
