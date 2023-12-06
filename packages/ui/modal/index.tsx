5357a3936fe66799957adc0d204778886f5975b3
ChayMyungjune <acg70159@gmail.com>
2023-12-07 01:54:10 +0900

    [Feat] Modal overaly seperate and color theme format setup

--- packages/ui/modal/index.tsx
+++ packages/ui/modal/index.tsx
@@ -3,7 +3,7 @@
 import { ReactNode, useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
-import SearchModalOverlay from './overlay'
+import SearchModalOverlay from '../../../apps/finance/components/organisms/Nav/SearchModal/overlay'
 
 const PortalModal = ({ children }: React.PropsWithChildren) => {
     const [domReady, setDomReady] = useState<any>(null)
@@ -13,7 +13,6 @@ const PortalModal = ({ children }: React.PropsWithChildren) => {
     return (
         domReady && createPortal(
             <>
-                <SearchModalOverlay />
                 {children}
             </>,
             domReady
